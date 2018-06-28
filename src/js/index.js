91b0b7783204c6e248489a11cc8cbe947126bb68
habeezzy <mhabeebulah@gmail.com>
2018-06-28 14:58:15 -0400

    javascript menu, contact form PHP?

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,35 @@ import img1 from '../img/img1.jpg'
 import img2 from '../img/img2.jpg'
 import itJS from '../img/itJS.jpg'
 import LIMM from '../img/LIMM.jpg'
+
+//////////////////
+
+$(document).ready(function(){
+  $('.fa').click(function() {
+    $('.home').toggleClass('menu-active');
+  })
+});
+
+$(document).ready(function(){
+  $('.fa').click(function() {
+    $('.resume').toggleClass('menu-active');
+  })
+});
+
+$(document).ready(function(){
+  $('.fa').click(function() {
+    $('.contact').toggleClass('menu-active');
+  })
+});
+
+$(document).ready(function(){
+  $('.fa').click(function() {
+    $('.work').toggleClass('menu-active');
+  })
+});
+
+$(document).ready(function() {
+    $('.fa').click(function() {
+      $('i').toggleClass('fa-bars fa-times');
+    });
+  });
