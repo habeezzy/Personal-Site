b2315e4e0d6384b479ca8936c22ec1fcab70a1fe
habeezzy <mhabeebulah@gmail.com>
2018-07-06 22:30:14 -0400

    final 3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/src/index.html')
 })
 //Create default port to serve the app on
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port)
 //log to feedback that this is actually running
 console.log('server started on port' + port)
