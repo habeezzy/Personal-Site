86bebf0e1105cf16b3b8c7a29076560033d4469d
habeezzy <mhabeebulah@gmail.com>
2018-08-01 17:13:18 -0400

    server and package json config for deployment

--- dist/work.bc27eba5.js
+++ dist/work.bc27eba5.js
@@ -1,16 +1,16 @@
-parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({11:[function(require,module,exports) {
+parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({9:[function(require,module,exports) {
 module.exports="/img3.21232dc7.jpg";
-},{}],15:[function(require,module,exports) {
-module.exports="/LIMM.572fe28a.jpg";
-},{}],14:[function(require,module,exports) {
-module.exports="/itJS.8f28ac42.jpg";
-},{}],9:[function(require,module,exports) {
-
-},{"./..\\img\\img3.jpg":11,"./..\\img\\LIMM.jpg":15,"./..\\img\\itJS.jpg":14}],12:[function(require,module,exports) {
-module.exports="/img1.b98c9d68.jpg";
 },{}],13:[function(require,module,exports) {
+module.exports="/LIMM.572fe28a.jpg";
+},{}],12:[function(require,module,exports) {
+module.exports="/itJS.8f28ac42.jpg";
+},{}],7:[function(require,module,exports) {
+
+},{"./..\\img\\img3.jpg":9,"./..\\img\\LIMM.jpg":13,"./..\\img\\itJS.jpg":12}],10:[function(require,module,exports) {
+module.exports="/img1.b98c9d68.jpg";
+},{}],11:[function(require,module,exports) {
 module.exports="/img2.75c1c173.jpg";
-},{}],5:[function(require,module,exports) {
+},{}],6:[function(require,module,exports) {
 "use strict";require("../scss/main.css"),require("../scss/main.scss");var i=require("../img/img3.jpg"),e=j(i),r=require("../img/img1.jpg"),s=j(r),g=require("../img/img2.jpg"),u=j(g),m=require("../img/itJS.jpg"),q=j(m),c=require("../img/LIMM.jpg"),t=j(c);function j(i){return i&&i.__esModule?i:{default:i}}
-},{"../scss/main.css":9,"../scss/main.scss":9,"../img/img3.jpg":11,"../img/img1.jpg":12,"../img/img2.jpg":13,"../img/itJS.jpg":14,"../img/LIMM.jpg":15}]},{},[5])
+},{"../scss/main.css":7,"../scss/main.scss":7,"../img/img3.jpg":9,"../img/img1.jpg":10,"../img/img2.jpg":11,"../img/itJS.jpg":12,"../img/LIMM.jpg":13}]},{},[6])
 //# sourceMappingURL=/work.80e57cc5.map
--- server.js
+++ server.js
@@ -1,16 +1,44 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-//create the express app
-const app = express()
-//create middleware to handle serving the app
-app.use("/", serveStatic (path.join (__dirname, '/dist')))
-//serve index by default
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
-})
-//Create default port to serve the app on
-const port = process.env.PORT || 4000;
-app.listen(port)
-//log to feedback that this is actually running
-console.log('server started on port' + port)
+/* eslint-disable */
+var express = require('express');
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started port: ${port}`);
+  }
+});
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const serveStatic = require('serve-static')
+// const path = require('path')
+// //create the express app
+// const app = express()
+// //create middleware to handle serving the app
+// app.use("/", serveStatic (path.join (__dirname, '/dist')))
+// //serve index by default
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/dist/index.html')
+// })
+// //Create default port to serve the app on
+// const port = process.env.PORT || 4000;
+// app.listen(port)
+// //log to feedback that this is actually running
+// console.log('server started on port' + port)
