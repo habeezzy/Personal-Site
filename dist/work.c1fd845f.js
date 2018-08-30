e1f0ed0dcd36e5a0c865b804bdbc59aecdbcb1d5
habeezzy <mhabeebulah@gmail.com>
2018-08-30 16:10:44 -0700

    build

---
+++ dist/work.c1fd845f.js
@@ -0,0 +1,18 @@
+parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({9:[function(require,module,exports) {
+module.exports="/img3.21232dc7.jpg";
+},{}],13:[function(require,module,exports) {
+module.exports="/LIMM.572fe28a.jpg";
+},{}],12:[function(require,module,exports) {
+module.exports="/itJS.8f28ac42.jpg";
+},{}],15:[function(require,module,exports) {
+
+},{"./..\\img\\img3.jpg":9,"./..\\img\\LIMM.jpg":13,"./..\\img\\itJS.jpg":12}],14:[function(require,module,exports) {
+module.exports="/HTMLresponsive.fe97beec.jpg";
+},{}],10:[function(require,module,exports) {
+module.exports="/img1.b98c9d68.jpg";
+},{}],11:[function(require,module,exports) {
+module.exports="/img2.75c1c173.jpg";
+},{}],6:[function(require,module,exports) {
+"use strict";require("../scss/main.css"),require("../scss/main.scss");var e=require("../img/img3.jpg"),i=t(e),r=require("../img/img1.jpg"),s=t(r),g=require("../img/img2.jpg"),u=t(g),m=require("../img/itJS.jpg"),q=t(m),p=require("../img/LIMM.jpg"),c=t(p),j=require("../img/HTMLresponsive.jpg"),n=t(j);function t(e){return e&&e.__esModule?e:{default:e}}
+},{"../scss/main.css":15,"../scss/main.scss":15,"../img/img3.jpg":9,"../img/img1.jpg":10,"../img/img2.jpg":11,"../img/itJS.jpg":12,"../img/LIMM.jpg":13,"../img/HTMLresponsive.jpg":14}]},{},[6])
+//# sourceMappingURL=/work.d0d6686e.map
--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@ import img1 from '../img/img1.jpg'
 import img2 from '../img/img2.jpg'
 import itJS from '../img/itJS.jpg'
 import LIMM from '../img/LIMM.jpg'
+import HTML from '../img/HTMLresponsive.jpg'
 
 //////////////////
 
